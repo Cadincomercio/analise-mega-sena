46f53a0f5094e97cb9cd11d1245df2b90c9964c1
Cadincomercio <cadincomercio@gmail.com>
2025-05-07 15:08:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-""import streamlit as st
+import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -87,4 +87,4 @@ if uploaded_file:
 
 else:
     st.warning('Por favor, carregue um arquivo para iniciar a análise.')
-""
+
